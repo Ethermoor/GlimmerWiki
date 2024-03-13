8389151833eb09cece169f38e6633b7588ace2a0
Sanan Rao <raosanan@gmail.com>
2024-03-13 02:00:25 -0400

    New Spawn Particle
    - Added new spawn particle API method

--- src/main/java/com/ethermoor/GlimmerAPI.java
+++ src/main/java/com/ethermoor/GlimmerAPI.java
@@ -13,7 +13,16 @@ public interface GlimmerAPI {
     void emitEmitter(List<Player> observers, String blueprint, Location location, String particle, int amount);
 
     void spawnEmitter(List<Player> observers, String blueprint, Location location);
-
-    void spawnParticle(List<Player> observers, String blueprint, Location origin, int count, double offsetX, double offsetY, double offsetZ);
+    void spawnTextParticle(List<Player> observers, String blueprint, String text, Location origin, boolean surface,
+                           int count, double sizeX, double sizeY, double sizeZ,
+                           double offsetX, double offsetY, double offsetZ);
+    void spawnParticle(List<Player> observers, String blueprint, Location origin, int count, double sizeX, double sizeY, double sizeZ);
+    void spawnParticle(List<Player> observers, String blueprint, Location origin,
+                       boolean surface,
+                       int count, double sizeX, double sizeY, double sizeZ,
+                       double offsetX, double offsetY, double offsetZ);
+    void spawnParticleSphere(List<Player> observers, String blueprint, Location origin,
+                          boolean surface, int count, double radius,
+                          double offsetX, double offsetY, double offsetZ);
 
 }
